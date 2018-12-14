b52defdac33ee0cf68a29002820000e81f2f8303
Chang <liuchangzju@gmail.com>
2018-12-14 14:59:52 +0100

    image provider start

--- img_provider.py
+++ img_provider.py
@@ -3,6 +3,7 @@ data provider for custom training process
 '''
 import os, glob
 import cv2
+import numpy as np
 
 TRAINING_PATH = os.path.join(
     os.path.dirname(__file__),
@@ -10,11 +11,11 @@ TRAINING_PATH = os.path.join(
 )
 
 class data_provider:
-    def __init__(self, batch_num):
+    def __init__(self, batch_num, data_shuffle = True):
         self.nbatch = batch_num
         self.img_names = glob.glob(os.path.join(TRAINING_PATH, '*.jpg'))
         self.img_num = len(self.img_names)
-
+        self.data_shuffle = data_shuffle
 
     def _next_training_batch(self):
         '''
@@ -24,6 +25,9 @@ class data_provider:
         :return:[input tensor: shape=[batch_num, x, y, channels],
                 output tensor: shape=[batch_num, x, y, classes]
         '''
+        img_index = np.random.randint(self.img_num, size=self.nbatch)
 
+        if self.data_shuffle:
+            np.random.shuffle(img_index)
 
-
+        
